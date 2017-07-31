f3b793df1e31ecd76b6bf0b85ffb3bc1720e4aeb
kerentang <1950655762@qq.com>
2017-07-31 13:30:08 +0800

    修复右括号提交两次的错误

--- yiyu/yiyuapp/src/components/Calculator.vue
+++ yiyu/yiyuapp/src/components/Calculator.vue
@@ -66,6 +66,7 @@ export default {
   data () {
     return {
       calculNum: 0,
+      lastBiope: null, // 保存上一个操作符
       exp: '', // 连接输入的数字和小数点，通过calculNum = parseFloat(exp) 转化成number
       flag: false, // 输入表达式的过程中用来判断中断输入时的依据：是否按下了=号，是否操作了单目运算
       s1: [], // 保存逆波兰表达式的栈
@@ -81,10 +82,12 @@ export default {
   },
   watch: {
     exp: function (newexp) {
+      // 判断正常结构的exp如2.34等的正则
       let reg = /^\d+(?:\.[1,9])?$/g
       if (reg.test(newexp)) {
         this.calculNum = parseFloat(newexp)
       } else if (newexp === '') {
+        // 没有输入数字或者清空输入时exp === ''则 calculNum === 0
         this.calculNum = 0
       } else {
         this.calculNum = newexp
@@ -93,10 +96,12 @@ export default {
   },
   methods: {
     typeExp (data) {
+      // 判断是否中断了当前输入
       this.checkFlag()
       if (this.exp.indexOf('.') > -1 && data === '.') {
         this.exp = this.exp
       } else if (this.exp === '' && data === '.') {
+        // exp不能以.开头
         this.exp = ''
         this.$message('请先输入数字！')
       } else {
@@ -106,9 +111,11 @@ export default {
     typeSingleOpe (data) {
       this.siOperator = data
       this.calSingleOpe(this.siOperator)
+      // 单目计算时直接处理的是this.calculNum，此时this.exp！== this.calculNum，要强制相等，注意：exp是字符串
       this.exp = '' + this.calculNum
       this.flag = true
     },
+    // 单目计算this.calculNum
     calSingleOpe (ope) {
       switch (ope) {
         case '+/-':
@@ -140,22 +147,33 @@ export default {
     },
     typeBiOpe (data) {
       this.flag = false
+      this.lastBiope = this.biOperator
       this.biOperator = data
-      if (this.biOperator !== '(') {
-        this.s1.push(this.calculNum)
+      console.log('this.lastBiope: ' + this.lastBiope + ', this.biOperator: ' + this.biOperator)
+      // 双目运算符 ) 后面不是等号就是双目运算符，为了不push两次，先进行判断
+      if (this.lastBiope !== ')') {
+        // 双目运算符（ 前面也是双目运算符，为了不push两次，先进行判断
+        if (data !== '(') {
+          this.s1.push(this.calculNum)
+        }
       }
+      console.log(this.s1)
       this.pushBiopeToS2(data)
       this.exp = ''
     },
     pushBiopeToS2 (curOpe) {
+      // 操作符是（直接push进s2
       if (curOpe === '(') {
         this.s2.push(curOpe)
       } else if (curOpe === ')') {
+        // 操作符是）且s2不为[],将s2中的操作符pop然后push到s1，直到遇到（
         while (this.s2[this.s2.length - 1] !== '(' && this.s2.length !== 0) {
           this.s1.push(this.s2.pop())
         }
+        // pop出（ ，不push到s1
         this.s2.pop()
       } else {
+        // 其他双目操作符：+-*／出栈s2入栈s1
         this.checkBiOpe(curOpe)
       }
     },
@@ -174,6 +192,7 @@ export default {
         }
       }
     },
+    // 从左到右遍历s1中的数字push到s3
     calculateS3 () {
       for (let item of this.s1) {
         if (/\d+/.test(item)) {
@@ -183,6 +202,7 @@ export default {
         }
       }
     },
+    // 计算逆波兰表达式：+-*／部分的计算
     popS3 (biope) {
       let right = parseFloat(this.s3.pop())
       let left = parseFloat(this.s3.pop())
@@ -203,14 +223,17 @@ export default {
       }
       this.s3.push(temp)
     },
+    // 计算最终的结果
     calculateResult () {
       this.flag = true
+      // 按下=时将this.calculNum存入s1,除了），因为 ）是双目运算符，有可能是一个表达式的结尾
       if (this.biOperator !== ')') {
         this.s1.push(this.calculNum)
       }
       while (this.s2.length !== 0) {
         this.s1.push(this.s2.pop())
       }
+      console.log('calcul s1: ' + this.s1)
       this.calculateS3()
       this.exp = '' + this.s3[0]
       this.s1 = []
